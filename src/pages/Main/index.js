6b11151c26e41f0723d8350fcef42bf2fed06d72
marcelo.zanol <marcelo.zanol@focco.com.br>
2020-04-01 22:39:31 -0300

    Add user list, navigation

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,21 +1,64 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { Keyboard, ActivityIndicator } from 'react-native';
+import AsyncStorage from '@react-native-community/async-storage';
 import Icon from 'react-native-vector-icons/MaterialIcons';
 import api from '../../services/api';
 
-import { Container, Form, Input, SubmitButton } from './styles';
+import {
+    Container,
+    Form,
+    Input,
+    SubmitButton,
+    List,
+    User,
+    Avatar,
+    Name,
+    Bio,
+    ProfileButton,
+    ProfileButtonText,
+} from './styles';
 
 export default class Main extends Component {
+    static navigationOptions = {
+        title: 'Usuários',
+    };
+
+    static propTypes = {
+        navigation: PropTypes.shape({
+            navigate: PropTypes.func,
+        }).isRequired,
+    };
+
     state = {
         newUser: '',
         users: [],
+        loading: false,
     };
+
+    async componentDidMount() {
+        const users = await AsyncStorage.getItem('users');
+
+        if (users) {
+            this.setState({ users: JSON.parse(users) });
+        }
+    }
+
+    componentDidUpdate(_, prevState) {
+        const { users } = this.state;
+
+        if (prevState.users !== users) {
+            AsyncStorage.setItem('users', JSON.stringify(users));
+        }
+    }
 
     handleAddUser = async () => {
         const { users, newUser } = this.state;
-        console.tron.log('antes do response');
-        const response = await api.get(`https://api.github.com/users/zanolera`);
-        // const response = await api.get(`/users/${newUser}`);
-        console.tron.log('depois do response');
+
+        this.setState({ loading: true });
+
+        const response = await api.get(`/users/${newUser}`);
+
         const data = {
             name: response.data.name,
             login: response.data.login,
@@ -26,11 +69,20 @@ export default class Main extends Component {
         this.setState({
             users: [...users, data],
             newUser: '',
+            loading: false,
         });
+
+        Keyboard.dismiss();
+    };
+
+    handleNavigate = user => {
+        const { navigation } = this.props;
+
+        navigation.navigate('User', { user });
     };
 
     render() {
-        const { users, newUser } = this.state;
+        const { users, newUser, loading } = this.state;
         return (
             <Container>
                 <Form>
@@ -43,15 +95,38 @@ export default class Main extends Component {
                         returnKeyType="send"
                         onSubmitEditing={this.handleAddUser}
                     />
-                    <SubmitButton onPress={this.handleAddUser}>
-                        <Icon name="add" size={20} color="#FFF" />
+                    <SubmitButton
+                        loading={loading}
+                        onPress={this.handleAddUser}
+                    >
+                        {loading ? (
+                            <ActivityIndicator color="#FFF" />
+                        ) : (
+                            <Icon name="add" size={20} color="#FFF" />
+                        )}
                     </SubmitButton>
                 </Form>
+
+                <List
+                    data={users}
+                    keyExtractor={user => user.login}
+                    renderItem={({ item }) => (
+                        <User>
+                            <Avatar source={{ uri: item.avatar }} />
+                            <Name>{item.name}</Name>
+                            <Bio>{item.bio}</Bio>
+
+                            <ProfileButton
+                                onPress={() => this.handleNavigate(item)}
+                            >
+                                <ProfileButtonText>
+                                    Ver perfil
+                                </ProfileButtonText>
+                            </ProfileButton>
+                        </User>
+                    )}
+                />
             </Container>
         );
     }
 }
-
-Main.navigationOptions = {
-    title: 'Usuários',
-};
